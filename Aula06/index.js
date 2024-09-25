0f5565bfab7a54320b278dab5d75e8f54d16faeb
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-24 21:21:05 -0300

    att

---
+++ Aula06/index.js
@@ -0,0 +1,19 @@
+// Observações
+// Não podemos começar variáveis com palavras reservadas
+// Variáveis precisam ter nome significarivos
+// Não pode começar o nome de uma variável com um número
+// As variáveis não podem conter espaços ou traços.
+// Utilizamos camelCase
+// case-Sensitive
+// Não podemos redeclarar variáveis com LET
+// Não utilizar Var e sim LET
+
+
+let nomeCompleto = 'Renato'
+
+
+console.log( nomeCompleto,'nasceu em 1984')
+console.log('Em 2000',nomeCompleto,'conheceu Maria')
+console.log( nomeCompleto,'casou-se com Maria em 2012')
+console.log('Maria teve 1 filho com',nomeCompleto,'em 2015.')
+console.log('O filho de',nomeCompleto,'se chama Eduardo.')

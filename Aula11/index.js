27f45fedea0ddf4dd17e99b80026626beeb34099
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-27 19:57:30 -0300

    att

--- Aula11/index.js
+++ Aula11/index.js
@@ -8,6 +8,37 @@
 %: Resto da Divisão
 
 */
-const num = 10
-const num2 = 5
-console.log(num % num2)
+
+/* Ordem de precedencia
+() 
+**
+* / %
++ -
+
+*/
+
+
+/*
+
+++ = Operadores de incremento  
+-- = Operadores de decremento
+
+Operadores de atribuição
+*=
++=
+/=
+ 
+
+// NaN - Not a number
+// parseInt('5') faz a conversão de string para número inteiro
+// parseFloat('5.2') faz a conversão de string para número com casas décimais
+// Number('5.2') faz a conversão de string para número, embora seja inteiro ou fracionado
+
+
+*/
+
+
+let num = 10
+let num2 = parseFloat('27.7')
+console.log(num+num2)
+console.log(typeof num2)

b91aa05ba68a581452a1a274c540bd33c2de1fb8
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-28 13:13:33 -0300

    att

---
+++ index.js
@@ -0,0 +1,18 @@
+let varA = 'A'
+let varB = 'B'
+let varC = 'C'
+  
+
+ const varATemp = varA   
+ varA = varB
+ varB = varC
+ varC = varATemp
+
+
+console.log(varA, varB, varC)
+
+/*
+
+[varA, varB, varC] = [varB, varC, varA]
+ 
+*/

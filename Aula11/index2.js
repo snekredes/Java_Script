66a30e73fd4fac25ec183ea1dfe1a1c96acf3714
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-28 12:43:31 -0300

    att

---
+++ Aula11/index2.js
@@ -0,0 +1,7 @@
+let num1 = window.prompt('Digite um número')
+let num2 = window.prompt('Digite outro número')
+num1 = Number(num1)
+num2 = Number(num2)
+let resultado = num1 + num2
+window.alert( `O Resultado da sua conta seria: ${resultado} !`)
+console.log(`O Resultado da sua conta seria: ${resultado} !`) 

442e49c6024626a6a214eb4d40961840f40daccd
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-10-03 22:01:10 -0300

    att

---
+++ Aula14/index.js
@@ -0,0 +1,30 @@
+let num1 = 0.7
+let num2 = 0.1
+
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+
+num1 = Number(num1.toFixed(2))
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+
+
+// converte o número em string somente aonde for usado -> console.log(num1.toString() + num2) 
+// converte o número em string somente aonde for usado -> num1 = num1.toString()//
+// converte em número binário -> console.log(num1.toString(6)) 
+// fixa os valores quando tem várias casas décimais ->  console.log(num1.toFixed(2))
+// identifica se o valor da várial é inteiro  -> console.log(Number.isInteger(num1))
+// identifica quando o valor for NAN sé verdadeira ou falso -> console.log(Number.isNaN(temp))
+ 
